97a077999da029f066884c5e208c7ff7a04ec370
noonbi <noonbi715@gmail.com>
2023-06-08 13:10:30 +0900

    file update

---
+++ js/wallet.js
@@ -0,0 +1,20 @@
+const WALLET_KEY = "WALLET_KEY";
+const WALLET_ADDRESS = "WALLET_ADDRESS";
+const walletAddress = document.querySelector("#textAddress");
+
+function saveData() {
+  var wkey = "1MZ7dWX3Gu8kLwjs7nRcUW2MHLnzC6QkGa";
+  localStorage.setItem(WALLET_ADDRESS, JSON.stringify(wkey));
+}
+
+function loadData() {
+  waddress = localStorage.getItem(WALLET_ADDRESS);
+  if (waddress) {
+    walletAddress.innerText = waddress;
+    //alert("Data loaded successfully: " + waddress);
+  } else {
+    waddress = "1MZ7dWX3Gu8kLwjs7nRcUW2MHLnzC6QkGa";
+  }
+}
+
+loadData();
