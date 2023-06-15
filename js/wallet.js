4f74ae81763a5397b697929c1e16330f0a142185
noonbi <noonbi715@gmail.com>
2023-06-15 16:45:19 +0900

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -2,9 +2,6 @@ const webAddress = document.getElementById("webAddress");
 const spanWA = document.getElementById("hiddenWA");
 const spanWK = document.getElementById("hiddenWK");
 
-const savedAddress = localStorage.getItem("WALLET_ADDRESS");
-const savedKey = localStorage.getItem("WALLET_KEY");
-
 function copyText() {
   const textToCopy = webAddress.innerText;
   navigator.clipboard
@@ -22,18 +19,17 @@ function initData() {
   localStorage.setItem(WALLET_ADDRESS, wkey);
 }
 
-//function clickButton() {
-//  var button = document.getElementById("hiddenButton");
-//  button.click();
-//  console.log("button-click");
-//}
-
-function sendData() {
-  var checkData = webAddress.innerText;
-  if (checkData == "") {
-    webAddress.innerText = savedAddress;
-    spanWA.value = savedAddress;
-    spanWK.value = savedKey;
-  }
-  console.log("savedAddress : ", savedAddress);
+function clickButton() {
+  var button = document.getElementById("hiddenButton");
+  button.click();
+  console.log("button-click");
 }
+
+var savedAddress = localStorage.getItem("WALLET_ADDRESS");
+var savedKey = localStorage.getItem("WALLET_KEY");
+spanWA.value = savedAddress;
+spanWK.value = savedKey;
+if savedAddress != "" {
+  clickButton();
+}
+
